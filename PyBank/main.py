6a4631827cbd8f769177e2932fa8eb3acebdfe44
Nicole Langford <nicolelangford@Nicoles-Air.attlocal.net>
2020-07-13 18:20:56 -0500

    added notes to file

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,7 @@ import csv
 
 budget_data_csv = os.path.join("Resources", "budget_data.csv")
 
+#create empty lists for data I need to show to live
 months = []
 profits_losses = []
 
@@ -10,6 +11,7 @@ profits_losses = []
 with open(budget_data_csv) as csvfile:
 	csvreader = csv.reader(csvfile, delimiter=",")
 	csv_header = next(csvfile)
+	# "next" makes sure that the program will move on to the next thing after the header
 
   #  print(csvr_header)
 	print(csv_header)
@@ -19,8 +21,11 @@ with open(budget_data_csv) as csvfile:
 		months.append(row[0])
 		profits_losses.append(int(row[1]))
 
+	# .append allows me to get at the specific thing I need in each row so that I can manipulate the data
+
 
 # Print Total number of months included in dataset
+# len function tells me how many things are in a list (diff from sum which tries to add integers)
 print("Total Number of Months Included in Dataset")
 print(len(months))
 
