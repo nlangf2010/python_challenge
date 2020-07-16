32b4b1763fcbd6a3ea460e9fd221615789c5b7a7
Nicole Langford <nicolelangford@Nicoles-Air.attlocal.net>
2020-07-16 09:19:46 -0500

    started analysis code in main_poll.py

---
+++ PyPoll/main_poll.py
@@ -0,0 +1,40 @@
+import os
+import csv
+
+#join to election data csv
+election_data = os.path.join("Resources", "election_data.csv")
+
+#create empty lists so we can manipulate data
+voter_id = []
+county = []
+candidate = []
+
+with open(election_data) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile) 
+
+    print(csv_header)
+
+    for row in csvreader:
+        #print(row[0], row[1], row[2])
+        voter_id.append(int(row[0]))
+        county.append(row[1])
+        candidate.append(row[2])
+
+#print header for results output
+print("Election Results")
+print("--------------------")
+
+#print total number of votes, using voter ID
+total_votes = len(voter_id)
+print(f"Total Votes: {total_votes}")
+
+#print separating line
+print("--------------------")
+
+
+
+
+
+
+
