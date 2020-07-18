4fb86cd22d0ed63db7785539806252fd0007bbb1
Nicole Langford <nicolelangford@Nicoles-Air.attlocal.net>
2020-07-17 23:19:17 -0500

    added code to find candidate vote count and percent

--- PyPoll/main_poll.py
+++ PyPoll/main_poll.py
@@ -36,9 +36,29 @@ print("--------------------")
 #get complete list of candidates who received votes
 candidate_list_set = set(candidate)
 unique_candidate_list = (list(candidate_list_set))
-for names in unique_candidate_list:
-    print(names)
-    
+#for names in unique_candidate_list:
+    #print(names)
+
+#get count and percentage of how many votes each candidate receieved by doing a list.count
+li_count = candidate.count("Li")
+li_percent = (li_count/total_votes)*100
+print(f'Li: {round(li_percent, 2)}% ({li_count})')
+
+khan_count = candidate.count("Khan")
+khan_percent = (khan_count/total_votes)*100
+print(f'Khan: {round(khan_percent, 2)}% ({khan_count})')
+
+correy_count = candidate.count("Correy")
+correy_percent = (correy_count/total_votes)*100
+print(f'Correy: {round(correy_percent, 2)}% ({correy_count})')
+
+otooley_count = candidate.count("O'Tooley")
+otooley_percent = (otooley_count/total_votes)*100
+print(f"O'Tooley: {round(otooley_percent, 2)}% ({otooley_count})")
+
+#find the winner of the election by popular vote
+
+
 
 
 
