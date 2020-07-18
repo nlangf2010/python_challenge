be62a6b801d586d8723ba26d0dcc00b4496ac3aa
Nicole Langford <nicolelangford@Nicoles-Air.attlocal.net>
2020-07-17 23:43:42 -0500

    added script to get winner and add .txt file

--- PyPoll/main_poll.py
+++ PyPoll/main_poll.py
@@ -1,5 +1,7 @@
 import os
 import csv
+import statistics
+from statistics import mode
 
 #join to election data csv
 election_data = os.path.join("Resources", "election_data.csv")
@@ -13,7 +15,7 @@ with open(election_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader) 
 
-    print(csv_header)
+    #print(csv_header)
 
     for row in csvreader:
         #print(row[0], row[1], row[2])
@@ -24,14 +26,14 @@ poll_data_zip = zip(voter_id, county, candidate)
 
 #print header for results output
 print("Election Results")
-print("--------------------")
+print("-------------------------")
 
 #print total number of votes, using voter ID
 total_votes = len(voter_id)
 print(f"Total Votes: {total_votes}")
 
 #print separating line
-print("--------------------")
+print("-------------------------")
 
 #get complete list of candidates who received votes
 candidate_list_set = set(candidate)
@@ -42,25 +44,39 @@ unique_candidate_list = (list(candidate_list_set))
 #get count and percentage of how many votes each candidate receieved by doing a list.count
 li_count = candidate.count("Li")
 li_percent = (li_count/total_votes)*100
-print(f'Li: {round(li_percent, 2)}% ({li_count})')
+print(f'Li: {round(li_percent, 3)}% ({li_count})')
 
 khan_count = candidate.count("Khan")
 khan_percent = (khan_count/total_votes)*100
-print(f'Khan: {round(khan_percent, 2)}% ({khan_count})')
+print(f'Khan: {round(khan_percent, 3)}% ({khan_count})')
 
 correy_count = candidate.count("Correy")
 correy_percent = (correy_count/total_votes)*100
-print(f'Correy: {round(correy_percent, 2)}% ({correy_count})')
+print(f'Correy: {round(correy_percent, 3)}% ({correy_count})')
 
 otooley_count = candidate.count("O'Tooley")
 otooley_percent = (otooley_count/total_votes)*100
-print(f"O'Tooley: {round(otooley_percent, 2)}% ({otooley_count})")
+print(f"O'Tooley: {round(otooley_percent, 3)}% ({otooley_count})")
+
+#print separting line
+print("-------------------------")
 
 #find the winner of the election by popular vote
+def popular_vote(candidate):
+    return(mode(candidate))
+print(f'Winner: {popular_vote(candidate)}')
 
+#print analysis table ending line
+print("-------------------------")
 
+#print to .txt file
 
-
+fh = open("poll_results.txt", "w")
+fh.write('Total Votes: ' + str(total_votes) + '\n' + 'Li: ' + str(round(li_percent, 3)) + " " + str(li_count) + '\n'
+          + 'Khan: ' + str(round(khan_percent, 3)) + " " + str(khan_count) + '\n'
+		  + 'Correy: ' + str(round(correy_percent, 3)) + " " + str(correy_count) + '\n'
+		  + "O'Tooley: " + str(round(otooley_percent, 3)) + " " + str(correy_count) + '\n' + "Winner: Khan")
+fh.close()
 
 
 
